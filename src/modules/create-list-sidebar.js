d7fc37c90e949812ab2141a9ccc692419e82ffe2
Isutomu <isutomu@gmail.com>
2024-05-16 20:27:49 -0300

    Create template for sidebar list element

---
+++ src/modules/create-list-sidebar.js
@@ -0,0 +1,38 @@
+import displayList from './display-list.js';
+import deleteList from './delete-list.js';
+
+// Main function
+export default function createListSidebar(id, name) {
+    const listElement = document.createElement('li');
+    listElement.setAttribute('data-list-id', id);
+    listElement.appendChild(createListInput(name));
+    listElement.appendChild(createListButton());
+
+    return listElement;
+};
+// ---
+
+// Complementary functions
+function createListInput(name) {
+    const listName = document.createElement('input');
+    listName.setAttribute('value', name);
+
+    listName.addEventListener('click', (e) => {
+        e.target.preventDefault();
+        displayList(inputListTab.closest('li').dataset.id);
+    });
+    listName.addEventListener('dblclick', (e) => e.target.focus());
+
+    return listName;
+};
+
+function createListButton() {
+    const deleteListBtn = document.createElement('button');
+    deleteListBtn.textContent = 'X';
+    deleteListBtn.addEventListener('click', (e) =>
+        deleteList(inputListTab.closest('li').dataset.id
+    ));
+
+    return deleteListBtn;
+};
+// ---
