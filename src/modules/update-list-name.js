5a680845699d8fe095cfc926edd2098cee25e5bf
Isutomu <isutomu@gmail.com>
2024-05-17 14:50:20 -0300

    Check if list that got name changed is in full display

--- src/modules/update-list-name.js
+++ src/modules/update-list-name.js
@@ -3,7 +3,10 @@ import { checkNameAvailability, setListNameStorage, getListName } from "./create
 export default function updateListName(listId, listName) {
     if(checkNameAvailability(listName)) {
         setListNameStorage(listId, listName);
-        document.querySelector('#header-list-name').textContent = listName;
+        const fullListHeader = document.querySelector('#header-list-name');
+        if(fullListHeader.closest('section').dataset.id === listId) {
+            fullListHeader.textContent = listName;
+        }
     } else {
         const listTabInput = document.querySelector(`#list-sidebar li[data-list-id="${listId}"] input`);
         listTabInput.setAttribute('disabled', '');
