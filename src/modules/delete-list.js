ad49bdc9818d1daf9ce7a6f5bd4fd8edef475d95
Isutomu <isutomu@gmail.com>
2024-05-18 15:46:19 -0300

    Fix selector from list elem on sidebar

--- src/modules/delete-list.js
+++ src/modules/delete-list.js
@@ -3,7 +3,7 @@ import displayList from './display-list.js';
 
 export default function deleteList(listId) {
     removeListStorage(listId);
-    document.querySelector(`#list-display li[data-list-id=${listId}]`).remove();
+    document.querySelector(`#list-sidebar li[data-list-id="${listId}"]`).remove();
 
     if(listId === document.querySelector('#list-display').dataset.id) {
         displayList('0');
